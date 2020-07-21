ddeca9219a170847bc9ee9ed4c72f6552f2c4f52
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-21 23:07:11 +0600

    DONE: Types for data and data have  been added

---
+++ src/store/types.ts
@@ -0,0 +1,12 @@
+export type ProductType = {
+  id: number;
+  name: string;
+  purchasePrice: number;
+  sellPrice: number;
+  category: string;
+};
+
+export type CategoryType = {
+  id: number;
+  name: string;
+};
