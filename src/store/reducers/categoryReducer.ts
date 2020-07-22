f33e45e7c03ef1f20ef4119724eede1873d70403
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-22 12:40:48 +0600

    DONE: Reducers have been implemented

--- src/store/reducers/categoryReducer.ts
+++ src/store/reducers/categoryReducer.ts
@@ -1,6 +1,24 @@
-import { CategoryType } from "../types";
+import {
+  CategoryType,
+  ADD_CATEGORY,
+  DELETE_CATEGORY,
+  FETCH_CATEGORIES,
+} from "../types";
 import { categories } from "../../services/products";
 
-const categoryReducer = (state: CategoryType[] = categories, action: any) => {};
+const categoryReducer = (state: CategoryType[] = categories, action: any) => {
+  switch (action.type) {
+    case ADD_CATEGORY:
+      return [...state, action.category];
+    case DELETE_CATEGORY:
+      return state.filter(
+        (category: CategoryType) => category.id !== action.id
+      );
+    case FETCH_CATEGORIES:
+        
+    default:
+      return state;
+  }
+};
 
 export default categoryReducer;
--- src/store/reducers/productReducer.ts
+++ src/store/reducers/productReducer.ts
@@ -1,6 +1,22 @@
-import { ProductType } from "../types";
+import {
+  ProductType,
+  ADD_PRODUCT,
+  DELETE_PRODUCT,
+  FETCH_PRODUCTS,
+} from "../types";
 import { products } from "../../services/products";
 
-const productReducer = (state: ProductType[] = products, action: any) => {};
+const productReducer = (state: ProductType[] = products, action: any) => {
+  switch (action.type) {
+    case ADD_PRODUCT:
+      return [...state, action.product];
+    case DELETE_PRODUCT:
+      return state.filter((product: ProductType) => product.id !== action.id);
+    case FETCH_PRODUCTS:
+
+    default:
+      return state;
+  }
+};
 
 export default productReducer;
