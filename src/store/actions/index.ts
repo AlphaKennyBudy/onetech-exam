08286671d2d47ce17ca00c9635df9e08018e901b
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-22 12:33:26 +0600

    DONE: Actions have been initialized

---
+++ src/store/actions/index.ts
@@ -0,0 +1,38 @@
+import {
+  ADD_CATEGORY,
+  DELETE_CATEGORY,
+  FETCH_CATEGORIES,
+  ProductType,
+  ADD_PRODUCT,
+  DELETE_PRODUCT,
+  CategoryType,
+  FETCH_PRODUCTS,
+} from "../types";
+
+export const addCategory = (category: CategoryType) => ({
+  type: ADD_CATEGORY,
+  category,
+});
+
+export const deleteCategory = (id: number) => ({
+  type: DELETE_CATEGORY,
+  id,
+});
+
+export const fetchCategories = () => ({
+  type: FETCH_CATEGORIES,
+});
+
+export const addProduct = (product: ProductType) => ({
+  type: ADD_PRODUCT,
+  product,
+});
+
+export const deleteProduct = (id: number) => ({
+  type: DELETE_PRODUCT,
+  id,
+});
+
+export const fetchProducts = () => ({
+  type: FETCH_PRODUCTS,
+});
