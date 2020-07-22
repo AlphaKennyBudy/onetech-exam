57ab0d5aa668db1c34a83118cd68f27eac4adcfa
AlphaKennyBudy <mr.adil777@gmail.com>
2020-07-22 12:23:53 +0600

    DONE: Renamed reducers into stores

--- src/store/reducers/rootReducer.ts
+++ src/store/reducers/rootReducer.ts
@@ -2,6 +2,9 @@ import { combineReducers } from "redux";
 import productReducer from "./productReducer";
 import categoryReducer from "./categoryReducer";
 
-const rootReducer = combineReducers({ productReducer, categoryReducer });
+const rootReducer = combineReducers({
+  productStore: productReducer,
+  categoryStore: categoryReducer,
+});
 
 export default rootReducer;
